8bbca9b71aa09cf99a3e93c08466682e01099720
student <tester@tester-m>
2019-02-24 10:07:16 +0100

    added 2 steps to register

--- test_wizzair.py
+++ test_wizzair.py
@@ -7,16 +7,26 @@ class WizzairTest(unittest.TestCase):
 
     """
     Class for registartion on wizzair.com
-    """"
-    def setUP(self):
-        pass
-
+    """
     def tearDown(self):
-        pass
+        self.driver.quit()
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get("https://wizzair.com/pl-pl#")
+
 
     def test_wrong_email(self):
-        pass
 
+        #złe podjeście - trudne w utrzymaniu - numer 28
+        zaloguj_btns =self.driver.find_elements_by_tag_name('button')
+        zaloguj_btns[28].click()
+
+        rejestracja_btn = self.driver.find_element_by_xpath('//*[@id="login-modal"]/form/div/p/button')
+        rejestracja_btn.click()
+
+        time.sleep(5)
 
 if __name__ =='__main__':
     unittest.main()
