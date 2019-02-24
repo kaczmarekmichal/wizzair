5764db056a9641591cadf786a4b2ec51c5892894
student <tester@tester-m>
2019-02-24 09:38:57 +0100

    added sketch for unittest

--- test_wizzair.py
+++ test_wizzair.py
@@ -1,4 +1,22 @@
 # _*_ coding: utf-8 _*_
-
+import unittest
 from selenium import webdriver
 import time
+
+class WizzairTest(unittest.TestCase):
+
+    """
+    Class for registartion on wizzair.com
+    """"
+    def setUP(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_wrong_email(self):
+        pass
+
+
+if __name__ =='__main__':
+    unittest.main()
