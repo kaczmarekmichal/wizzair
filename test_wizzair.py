b9db39b5565de9346d01f95088cd8574913253ec
student <tester@tester-m>
2019-02-24 11:05:47 +0100

    register step 1

--- test_wizzair.py
+++ test_wizzair.py
@@ -20,11 +20,30 @@ class WizzairTest(unittest.TestCase):
     def test_wrong_email(self):
 
         #złe podjeście - trudne w utrzymaniu - numer 28
-        zaloguj_btns =self.driver.find_elements_by_tag_name('button')
-        zaloguj_btns[28].click()
+        # zaloguj_btns =self.driver.find_elements_by_tag_name('button')
+        # zaloguj_btns[28].click()
+        # zaloguj_btns =self.driver.find_elements_by_class_name('navigation__button')
+        # zaloguj_btns.click()
 
-        rejestracja_btn = self.driver.find_element_by_xpath('//*[@id="login-modal"]/form/div/p/button')
+
+        zaloguj_btn =self.driver.find_element_by_xpath("//button[@data-test='navigation-menu-signin']")
+        #własny selctor na podstawie inspekcji strony w chromie - data-test poniewaz było jedne unikalne wystapienie
+        zaloguj_btn.click()
+        
+
+        rejestracja_btn =self.driver.find_element_by_xpath("//button[text()='Rejestracja']")
         rejestracja_btn.click()
+
+        # rejestracja_btn = self.driver.find_element_by_xpath('//*[@id="login-modal"]/form/div/p/button')
+        # rejestracja_btn.click()
+
+        input = self.driver.find_element_by_name("firstName")
+        input.send_keys("Dick")
+
+
+        # input = self.driver.find_element_by_name("lastName")
+        # input.send_keys("Dickinson")
+
 
         time.sleep(5)
 
